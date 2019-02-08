735e9d899cbb96c9ea158a836e90ce47f3c0cbe2
Renan Pallin <renanpallin@gmail.com>
2019-02-08 02:20:03 -0200

    Until stories BLOC

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: buildList(bloc),
+    );
+  }
+
+  buildList(StoriesBloc bloc) {
+    
+  }
+}
