d79c349bea3d6abcb15794afa805747093e2625b
Renan Pallin <renanpallin@gmail.com>
2019-02-15 17:37:52 -0200

    Scaffold in NewsDetail

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 class NewsDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('NewsDetail page');
+    return Scaffold(
+      /// Só de colocar o AppBar,
+      /// temos a setinha de voltar já
+      /// pois ele detecta que uma navegação
+      /// foi feita.
+      appBar: AppBar(
+        title: Text('News Detail'),
+      ),
+      body: Text('oi'),
+    );
   }
-
 }
