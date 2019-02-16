33b88efe1b0fd1fd85384d53facd0b3c3831693c
Renan Pallin <renanpallin@gmail.com>
2019-02-16 04:23:30 -0200

    Comments ident and html_unescape package

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
+/// Existe uma versão basic dessa lib
+import 'package:html_unescape/html_unescape.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
   final Map<int, Future<ItemModel>> itemMap;
+  final int depth;
 
-  Comment({this.itemId, this.itemMap});
+  Comment({this.itemId, this.itemMap, this.depth = 0});
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +25,28 @@ class Comment extends StatelessWidget {
         return Column(
           children: <Widget>[
             ListTile(
-              title: item.text == '' ? Text('Deleted') : Text(item.text),
+              title: buildText(item),
               subtitle: item.by == '' ? Text('Deleted') : Text(item.by),
+              contentPadding: EdgeInsets.only(
+                right: 16.0,
+                left: (depth + 1) * 16.0,
+              ),
             ),
             Divider(),
           ]..addAll(item.kids.map((kidId) {
               return Comment(
                 itemId: kidId,
                 itemMap: itemMap,
+                depth: depth + 1,
               );
             })),
         );
       },
     );
+  }
+
+  buildText(ItemModel item) {
+    final text = HtmlUnescape().convert(item.text);
+    return Text(text);
   }
 }
